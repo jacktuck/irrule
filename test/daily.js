ec3bd30597c68bbc81ea054ec6cb3396ba09b630
Jack Tuck <jack-tuck@hotmail.com>
2017-06-04 22:08:46 +0100

    Added bySecond to nextDay generator

--- test/daily.js
+++ test/daily.js
@@ -8,36 +8,78 @@ const expect = chai.expect
 describe('DAILY', function () {
   describe(`
     DTSTART;TZID=US-Eastern:19970902T090000
-    FREQ=DAILY;BYHOUR=9,10,11,12,13,14,15,16;BYMINUTE=0,20,40
+    FREQ=DAILY;BYHOUR=9,10,11,12,13,14,15,16;BYMINUTE=0,20,40;BYSECOND=30,40;COUNT=5
   `, function () {
     it('Recurring every 20 minutes from 9:00 AM to 4:40 PM every day', function () {
-      let occurances = new irrule({
+      let rrule = new irrule({
         freq: 'daily',
         byHour: [9,10,11,12,13,14,15,16],
-        byMinute: [0,20,40]
+        byMinute: [0,20,40],
+        bySecond: [30, 40],
+        until: 'Wed Sep 03 1997 09:40:50 GMT+0100'
       })
 
 
 
-      console.log(occurances.iter())
+      let occurances = rrule.all("1997-09-02T09:00:00.000")
+      let expected = [
+        'Tue Sep 02 1997 09:00:30 GMT+0100',
+        'Tue Sep 02 1997 09:00:40 GMT+0100',
+        'Tue Sep 02 1997 09:20:30 GMT+0100',
+        'Tue Sep 02 1997 09:20:40 GMT+0100',
+        'Tue Sep 02 1997 09:40:30 GMT+0100',
+        'Tue Sep 02 1997 09:40:40 GMT+0100',
+        'Tue Sep 02 1997 10:00:30 GMT+0100',
+        'Tue Sep 02 1997 10:00:40 GMT+0100',
+        'Tue Sep 02 1997 10:20:30 GMT+0100',
+        'Tue Sep 02 1997 10:20:40 GMT+0100',
+        'Tue Sep 02 1997 10:40:30 GMT+0100',
+        'Tue Sep 02 1997 10:40:40 GMT+0100',
+        'Tue Sep 02 1997 11:00:30 GMT+0100',
+        'Tue Sep 02 1997 11:00:40 GMT+0100',
+        'Tue Sep 02 1997 11:20:30 GMT+0100',
+        'Tue Sep 02 1997 11:20:40 GMT+0100',
+        'Tue Sep 02 1997 11:40:30 GMT+0100',
+        'Tue Sep 02 1997 11:40:40 GMT+0100',
+        'Tue Sep 02 1997 12:00:30 GMT+0100',
+        'Tue Sep 02 1997 12:00:40 GMT+0100',
+        'Tue Sep 02 1997 12:20:30 GMT+0100',
+        'Tue Sep 02 1997 12:20:40 GMT+0100',
+        'Tue Sep 02 1997 12:40:30 GMT+0100',
+        'Tue Sep 02 1997 12:40:40 GMT+0100',
+        'Tue Sep 02 1997 13:00:30 GMT+0100',
+        'Tue Sep 02 1997 13:00:40 GMT+0100',
+        'Tue Sep 02 1997 13:20:30 GMT+0100',
+        'Tue Sep 02 1997 13:20:40 GMT+0100',
+        'Tue Sep 02 1997 13:40:30 GMT+0100',
+        'Tue Sep 02 1997 13:40:40 GMT+0100',
+        'Tue Sep 02 1997 14:00:30 GMT+0100',
+        'Tue Sep 02 1997 14:00:40 GMT+0100',
+        'Tue Sep 02 1997 14:20:30 GMT+0100',
+        'Tue Sep 02 1997 14:20:40 GMT+0100',
+        'Tue Sep 02 1997 14:40:30 GMT+0100',
+        'Tue Sep 02 1997 14:40:40 GMT+0100',
+        'Tue Sep 02 1997 15:00:30 GMT+0100',
+        'Tue Sep 02 1997 15:00:40 GMT+0100',
+        'Tue Sep 02 1997 15:20:30 GMT+0100',
+        'Tue Sep 02 1997 15:20:40 GMT+0100',
+        'Tue Sep 02 1997 15:40:30 GMT+0100',
+        'Tue Sep 02 1997 15:40:40 GMT+0100',
+        'Tue Sep 02 1997 16:00:30 GMT+0100',
+        'Tue Sep 02 1997 16:00:40 GMT+0100',
+        'Tue Sep 02 1997 16:20:30 GMT+0100',
+        'Tue Sep 02 1997 16:20:40 GMT+0100',
+        'Tue Sep 02 1997 16:40:30 GMT+0100',
+        'Tue Sep 02 1997 16:40:40 GMT+0100',
+        'Wed Sep 03 1997 09:00:30 GMT+0100',
+        'Wed Sep 03 1997 09:00:40 GMT+0100',
+        'Wed Sep 03 1997 09:20:30 GMT+0100',
+        'Wed Sep 03 1997 09:20:40 GMT+0100',
+        'Wed Sep 03 1997 09:40:30 GMT+0100',
+        'Wed Sep 03 1997 09:40:40 GMT+0100'
+      ]
 
-
-      // let gen = occurances.next()(moment("1997-09-02T09:00:00.000"))
-
-      // console.log('gen', gen)
-
-      // _.times(25, gen.next.bind(gen))
-
-      // console.log('BANG', gen.next().value)
-
-      // expect(gen.next().value).to.equal(moment("1997-09-02T09:00:00.000").toString())
-      // expect(gen.next().value).to.equal(moment("1997-09-02T09:20:00.000").toString())
-      // expect(gen.next().value).to.equal(moment("1997-09-02T09:40:00.000").toString())
-      // expect(gen.next().value).to.equal(moment("1997-09-02T10:00:00.000").toString())
-      //
-
-
-      // check we get onto the next day by iterating by 24hrs (24 * 4)
+      expect(occurances).to.deep.equal(expected)
     })
   })
 })
